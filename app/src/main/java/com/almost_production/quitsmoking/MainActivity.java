7a0416c252c68704029024c7f52043a4671498ab
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-21 12:08:46 +0300

    Change images with time

--- app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
+++ app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
@@ -1,17 +1,10 @@
 package com.almost_production.quitsmoking;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.CountDownTimer;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,15 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RemoteViews;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.io.Console;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -56,83 +40,110 @@ public class MainActivity extends AppCompatActivity {
         sigarets_count = (EditText) findViewById(R.id.sigarets_count);
         count = Integer.parseInt(sigarets_count.getText().toString());
 
+        // Buttons events
         button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count = Integer.parseInt(sigarets_count.getText().toString());
-                LaunchTimer(count);
-                count += 5;
-                button_imsmoked.setEnabled(false);
-                button_imsmoked.setText("Wait");
+                ClickButton(true);
+                CustomNotification();
             }
         });
 
         button_imsmoked.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LaunchTimer(count);
-                count += 5;
-                button_imsmoked.setEnabled(false);
-                button_imsmoked.setText("Wait");
+                ClickButton(false);
             }
         });
-
-
-        // NOTIFICATION CODE
-//        button_smoke_not = (Button) findViewById(R.id.button_smoked_not);
-//
-//        int icon = R.mipmap.ic_launcher;
-//        long when = System.currentTimeMillis();
-//        notification = new Notification(icon, "Custom Notification", when);
-//
-//        mNotificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-//
-//        contentView = new RemoteViews(getPackageName(), R.layout.notification);
-//        contentView.setImageViewResource(R.id.image, R.mipmap.ic_launcher);
-//        contentView.setTextViewText(R.id.title, "Quit smoking");
-//        contentView.setTextViewText(R.id.text, "0 h 0 min");
-//        notification.contentView = contentView;
-//
-//        Intent notificationIntent = new Intent(this, MainActivity.class);
-//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-//        notification.contentIntent = contentIntent;
-//
-//        notification.flags |= Notification.FLAG_NO_CLEAR; //Do not clear the notification
-//
-//        mNotificationManager.notify(1, notification);
-//
-//        System.out.print("!!!" + button_smoke_not);
     }
 
-//    private static final int NOTIFY_ID = 101;
-//
-//    public void UpdateNotification(boolean canSmoke) {
-//        contentView.setTextViewText(R.id.text, textView.getText().toString());
-//
-//        if (canSmoke) {
-//            button_smoke_not.setEnabled(true);
-//        } else {
-//            button_smoke_not.setEnabled(false);
-//        }
-//
-//        mNotificationManager.notify(1, notification);
-//    }
+    RemoteViews remoteViews;
+    NotificationCompat.Builder builder;
+    NotificationManager notificationmanager;
+    public void CustomNotification() {
+        // Using RemoteViews to bind custom layouts into Notification
+        remoteViews = new RemoteViews(getPackageName(),
+                R.layout.notification);
 
+        // Set Notification Title
+        String strtitle = "notification title";
+        // Set Notification Text
+        String strtext = "notification text";
+
+        // Open NotificationView Class on Notification Click
+        Intent intent = new Intent(this, NotificationView.class);
+        // Send data to NotificationView Class
+        intent.putExtra("title", "Time left");
+        intent.putExtra("text", timeleft);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder = new NotificationCompat.Builder(this)
+                // Set Icon
+                .setSmallIcon(R.drawable.smoke)
+                // Set Ticker Message
+                .setTicker("Ticker message")
+                // Dismiss Notification
+                .setAutoCancel(true)
+                // Set PendingIntent into Notification
+                .setContentIntent(pIntent)
+                // Set RemoteViews into Notification
+                .setContent(remoteViews);
+
+        // Locate and set the Image into customnotificationtext.xml ImageViews
+        remoteViews.setImageViewResource(R.id.image,R.drawable.smoke);
+
+        // Locate and set the Text into customnotificationtext.xml TextViews
+        remoteViews.setTextViewText(R.id.title,"Time left");
+        remoteViews.setTextViewText(R.id.text,timeleft);
+
+        // Create Notification Manager
+        notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Build Notification with Notification Manager
+        notificationmanager.notify(0, builder.build());
+    }
+
+    public void UpdateNotification(boolean cansmoke) {
+        remoteViews.setTextViewText(R.id.text,timeleft);
+        if (cansmoke) {
+            remoteViews.setImageViewResource(R.id.image,R.drawable.smoke);
+            builder.setSmallIcon(R.drawable.smoke);
+        } else {
+            remoteViews.setImageViewResource(R.id.image,R.drawable.smoke_no);
+            builder.setSmallIcon(R.drawable.smoke_no);
+        }
+        notificationmanager.notify(0, builder.build());
+    }
+
+    public void ClickButton(boolean start) {
+        if (start)
+            count = Integer.parseInt(sigarets_count.getText().toString());
+        LaunchTimer(count);
+        count += 5;
+        button_imsmoked.setEnabled(false);
+        button_imsmoked.setText("Wait");
+    }
+
+    long minleft, secleft;
+    String timeleft;
     void LaunchTimer(int sec) {
 
         if (cd != null)
             cd.cancel();
-        cd = new CountDownTimer(sec * 1000, 100) {
+        cd = new CountDownTimer(sec * 1000, 500) {
             public void onTick(long millisUntilFinished) {
-                textView_timeLeft.setText("Time left: "  + millisUntilFinished / 1000 / 60 +
-                        " min " + (millisUntilFinished + 1000) / 1000 + " sec");
-                //UpdateNotification(false);
+                minleft = millisUntilFinished / 1000 / 60;
+                secleft = (millisUntilFinished + 1000) / 1000;
+                timeleft = minleft + " min " + secleft + " sec";
+                textView_timeLeft.setText("Time left: "  +  timeleft);
+                UpdateNotification(false);
             }
             public void onFinish() {
                 textView_timeLeft.setText("You can go smoke");
                 button_imsmoked.setEnabled(true);
                 button_imsmoked.setText("I'm smoked");
-                //UpdateNotification(true);
+                UpdateNotification(true);
             }
 
         };
---
+++ app/src/main/java/com/almost_production/quitsmoking/NotificationView.java
@@ -0,0 +1,53 @@
+package com.almost_production.quitsmoking;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.app.Activity;
+
+
+public class NotificationView extends Activity {
+    String title;
+    String text;
+    TextView txttitle;
+    TextView txttext;
+
+    Button button_smoked_not;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+
+        // Create Notification Manager
+        NotificationManager notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Dismiss Notification
+        notificationmanager.cancel(0);
+
+        // Retrive the data from MainActivity.java
+        Intent i = getIntent();
+
+        title = i.getStringExtra("title");
+        text = i.getStringExtra("text");
+
+        // Locate the TextView
+        txttitle = (TextView) findViewById(R.id.title);
+        txttext = (TextView) findViewById(R.id.text);
+
+        // Set the data into TextView
+        txttitle.setText(title);
+        txttext.setText(text);
+
+        button_smoked_not = (Button) findViewById(R.id.button_smoked_not);
+        button_smoked_not.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
