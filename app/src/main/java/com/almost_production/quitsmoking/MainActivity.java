1d2bedec018aa640aabb6c576893950fc3b9a02c
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-22 10:37:44 +0300

    Add BroadcastReceiver

--- app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
+++ app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
@@ -2,11 +2,15 @@ package com.almost_production.quitsmoking;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -55,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     RemoteViews remoteViews;
     NotificationCompat.Builder builder;
     NotificationManager notificationmanager;
@@ -62,14 +67,18 @@ public class MainActivity extends AppCompatActivity {
     public void CustomNotification() {
         //remoteViews = new RemoteViews(getPackageName(), R.layout.notification);
         // old intent
-        Intent intent = new Intent(this, MainActivity.class);
+        //Intent intent = new Intent(this, MainActivity.class);
         //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
         //        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 //        Intent iAction1 = new Intent(this, MyIntentService.class);
 //        iAction1.setAction(MyIntentService.ACTION1);
 //        pIntent = PendingIntent.getService(this, 0, iAction1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // INTENT LAST CHANCE
+        Intent intent = new Intent("Download_Cancelled");
+        pIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
 
         builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.smoke)
@@ -84,9 +93,15 @@ public class MainActivity extends AppCompatActivity {
 //        remoteViews.setTextViewText(R.id.title,"Time left");
 //        remoteViews.setTextViewText(R.id.text,timeleft);
 
-
         notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationmanager.notify(0, builder.build());
+    }
+
+    public static class DownloadCancelReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //ClickButton(false);
+        }
     }
 
     public void UpdateNotification(boolean cansmoke) {
@@ -96,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
             //remoteViews.setImageViewResource(R.id.image,R.drawable.smoke);
             builder.setSmallIcon(R.drawable.smoke);
             builder.addAction(R.drawable.smoke,"I'm smoked", pIntent);
+
         } else {
             //remoteViews.setImageViewResource(R.id.image,R.drawable.smoke_no);
             builder.setSmallIcon(R.drawable.smoke_no);
