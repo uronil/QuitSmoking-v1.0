58561d77c2ebd19350e883c929b38a3ac87dd580
ununoctium1187 <ununoctium1187@googlemail.com>
2017-08-22 10:56:20 +0300

    Notification button click added

--- app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
+++ app/src/main/java/com/almost_production/quitsmoking/MainActivity.java
@@ -20,14 +20,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView_descr;
-    TextView textView_timeLeft;
-    EditText sigarets_count;
-    Button button_start;
-    Button button_imsmoked;
+    static TextView textView_descr;
+    static TextView textView_timeLeft;
+    static EditText sigarets_count;
+    static Button button_start;
+    static Button button_imsmoked;
 
-    CountDownTimer cd = null;
-    int count;
+    static CountDownTimer cd = null;
+    static String timeleft;
+    static int count;
+    static long minleft, secleft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,68 +61,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    RemoteViews remoteViews;
-    NotificationCompat.Builder builder;
-    NotificationManager notificationmanager;
-    PendingIntent pIntent;
+    static NotificationCompat.Builder builder;
+    static NotificationManager notificationmanager;
+    static PendingIntent pIntent;
     public void CustomNotification() {
-        //remoteViews = new RemoteViews(getPackageName(), R.layout.notification);
-        // old intent
-        //Intent intent = new Intent(this, MainActivity.class);
-        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-        //        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        //pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-//        Intent iAction1 = new Intent(this, MyIntentService.class);
-//        iAction1.setAction(MyIntentService.ACTION1);
-//        pIntent = PendingIntent.getService(this, 0, iAction1, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // INTENT LAST CHANCE
-        Intent intent = new Intent("Download_Cancelled");
+        Intent intent = new Intent("Send");
         pIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        Intent intentToMain = new Intent(this, MainActivity.class);
+        PendingIntent pIntentToMain = PendingIntent.getActivity(this,0,intentToMain, PendingIntent.FLAG_UPDATE_CURRENT);
 
         builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.smoke)
                 .setAutoCancel(false)
                 .setContentTitle("Time left")
-                .setTicker("Waiting")
                 .setOngoing(true) // always in notificationBar
+                .setShowWhen(false)
+                .setContentIntent(pIntentToMain)
                 .setContentText(timeleft);
-         //       .setContent(remoteViews);
-
-//        remoteViews.setImageViewResource(R.id.image,R.drawable.smoke);
-//        remoteViews.setTextViewText(R.id.title,"Time left");
-//        remoteViews.setTextViewText(R.id.text,timeleft);
 
         notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationmanager.notify(0, builder.build());
     }
 
-    public static class DownloadCancelReceiver extends BroadcastReceiver {
+    public static class Receiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            //ClickButton(false);
+            ClickButton(false);
         }
     }
 
-    public void UpdateNotification(boolean cansmoke) {
-        //remoteViews.setTextViewText(R.id.text,timeleft);
+    static public void UpdateNotification(boolean cansmoke) {
         builder.setContentText(timeleft);
         if (cansmoke) {
-            //remoteViews.setImageViewResource(R.id.image,R.drawable.smoke);
             builder.setSmallIcon(R.drawable.smoke);
             builder.addAction(R.drawable.smoke,"I'm smoked", pIntent);
-
         } else {
-            //remoteViews.setImageViewResource(R.id.image,R.drawable.smoke_no);
             builder.setSmallIcon(R.drawable.smoke_no);
             builder.mActions.clear();
         }
         notificationmanager.notify(0, builder.build());
     }
 
-    public void ClickButton(boolean start) {
+    static public void ClickButton(boolean start) {
         if (start)
             count = Integer.parseInt(sigarets_count.getText().toString());
         LaunchTimer(count);
@@ -129,9 +112,7 @@ public class MainActivity extends AppCompatActivity {
         button_imsmoked.setText("Wait");
     }
 
-    long minleft, secleft;
-    String timeleft;
-    void LaunchTimer(int sec) {
+    static void LaunchTimer(int sec) {
 
         if (cd != null)
             cd.cancel();
